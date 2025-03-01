07d1de8726230789e7e3cd4f469420f0d2b432d8
adesopiann <adesopian884@gmail.com>
2025-03-01 20:25:05 +0700

    feat: add mobile responsive

--- resources/views/pages/admin/walikelas/edit.blade.php
+++ resources/views/pages/admin/walikelas/edit.blade.php
@@ -1,37 +1,50 @@
 @extends('components.layouts.admin.container')
+@section('title', 'Data Walikelas')
 
 @section('main')
     <section>
-        <form action="{{ route('walikelas.update', $walas->id) }}" method="POST" class="bg-white p-15 space-y-6 rounded-md border-[1.5px] border-[#D9D9D9] max-w-[750px]">
+        <div class="flex space-x-2 md:space-x-8 mb-6 mb:12">
+                <a href="{{ route('guru.index') }}" class="flex items-center gap-4">
+                    <div class="flex items-center justify-center bg-primary aspect-square rounded-md size-6">
+                        <img src="{{ asset('icons/chevron-left.svg') }}" width="8" height="8" alt="">
+                    </div>
+                    <p class=" md:text-base font-bold text-mist">Data Walikelas</p>
+                </a>
+            <a href="{{ route('guru.index') }}" class="flex items-center gap-4">
+                <img src="{{ asset('icons/ep_arrow-right-bold.svg') }}" width="8" height="8" alt="" class="w-4">
+            </a>
+            <p class="text-base font-bold">Edit Walikelas</p>
+        </div>
+        <form action="{{ route('walikelas.update', $walas->id) }}" method="POST" class="bg-white p-4 md:p-15 space-y-6 rounded-md border-[1.5px] border-[#D9D9D9] max-w-[750px]">
             @csrf
             @method('PUT')
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="kode" class="text-lg font-medium">Kode Walikelas</label>
-                <input type="text" name="kode" id="kode" value="{{ $walas->kode }}" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('kode') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="kode" class="text-sm md:text-lg font-medium">Kode Walikelas</label>
+                <input type="text" name="kode" id="kode" value="{{ $walas->kode }}" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('kode') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="nama" class="text-lg font-medium">Nama</label>
-                <input type="text" name="nama" id="nama" value="{{ $walas->nama }}" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('nama') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="nama" class="text-sm md:text-lg font-medium">Nama</label>
+                <input type="text" name="nama" id="nama" value="{{ $walas->nama }}" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('nama') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="username" class="text-lg font-medium">Username</label>
-                <input type="username" name="username" id="username" value="{{ $walas->username }}" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('username') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="username" class="text-sm md:text-lg font-medium">Username</label>
+                <input type="username" name="username" id="username" value="{{ $walas->username }}" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('username') {{ $message }} @enderror</p>
             </div>
-             <div class="grid grid-cols-4 gap-3 items-center">
-                    <label for="kode_kelas" class="text-lg font-medium">Kelas</label>
-                    <select name="kode_kelas" id="kode_kelas" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+             <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                    <label for="kode_kelas" class="text-sm md:text-lg font-medium">Kelas</label>
+                    <select name="kode_kelas" id="kode_kelas" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
                         @foreach (App\Models\Kelas::orderBy('kode')->get() as $item)
                             <option value="{{ $item->kode }}">{{ $item->kode }}</option>
                         @endforeach
                     </select>
-                    <p class="col-start-2 text-red-500 text-sm">@error('kode_kelas') {{ $message }} @enderror</p>
+                    <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('kode_kelas') {{ $message }} @enderror</p>
                 </div>
             <div class="flex items-center justify-end gap-4 pt-4">
-                <a href="{{ route('walikelas.index') }}" class="bg-mist py-2 px-2.5 rounded text-white text-sm font-medium">Batal</a>
-                <button type="submit" class="bg-primary py-2 px-2.5 rounded text-white text-sm font-medium">Simpan</button>
+                <a href="{{ route('walikelas.index') }}" class="bg-mist  py-1 md:py-2 px-5 md:px-2.5 rounded text-white text-sm font-medium">Batal</a>
+                <button type="submit" class="bg-primary  py-1 md:py-2 px-5 md:px-2.5 rounded text-white text-sm font-medium">Simpan</button>
             </div>
         </form>
     </section>
--- resources/views/pages/admin/walikelas/index.blade.php
+++ resources/views/pages/admin/walikelas/index.blade.php
@@ -1,16 +1,31 @@
 @extends('components.layouts.admin.container')
+@section('title', 'Data Walikelas')
 
 @section('main')
     <section>
-         <div class="mb-[80px]">
+         <div class="mb-6 md:mb-12">
             <div class="flex gap-x-4 items-center">
-                <a href="{{ route('siswa.index') }}"><i data-feather="chevron-left" class="bg-primary rounded-[5px]  text-white"></i></a>
-                <p class="text-2xl text-gray-500 font-bold">Data Walikelas</p>
+                <a href="{{ url()->previous() }}"><i data-feather="chevron-left" class="bg-primary rounded-[5px]  text-white"></i></a>
+                <p class="text-sm md:text-base font-bold">Data Walikelas</p>
             </div>
         </div>
-        <div class="flex items-center justify-between">
+        <div class="flex flex-col space-y-6 md:space-y-0 md:items-center md:justify-between md:flex-row">
+            <form action="{{ route('walikelas.index') }}" method="GET" class="flex items-center gap-2.5 w-full md:w-auto">
+                <div class="relative w-full md:w-auto">
+                    <img src="{{ asset('icons/map_mosque.svg') }}" width="18" height="18" alt="" class="absolute top-1/2 -translate-y-1/2 left-2.5">
+                    <input type="search" name="search" value="{{ request('search') }}" placeholder="Cari Walikelas..." class="w-full md:w-auto bg-white rounded-full pl-9 pr-2.5 py-1 border border-primary">
+                </div>
+                <button class="bg-primary rounded-full px-5 py-2.5 text-white text-xs font-semibold">
+                    Cari
+                </button>
+                @if(request('search'))
+                <a href="{{ route('walikelas.index') }}" class="bg-gray-500 text-white rounded-full px-5 py-2.5 text-xs font-semibold">
+                    Reset
+                </a>
+                @endif
+            </form>
             <div class="flex items-center gap-2.5">
-                <a href="walikelas/create" class="flex items-center gap-4 px-5 py-2 rounded bg-transparent border border-primary text-primary">
+                <a href="walikelas/create" class="flex items-center gap-4  px-5 py-2 rounded bg-transparent border border-primary text-primary">
                     <img src="{{ asset('icons/map_mosque.svg') }}" alt="mosque icon" width="20" height="20">
                     <p class="text-sm font-bold">Tambah Data</p>
                 </a>
@@ -19,40 +34,26 @@
                     'action' => route('walikelas.import')
                 ])
             </div>
-           <form action="{{ route('walikelas.index') }}" method="GET" class="flex items-center gap-2.5">
-                <div class="relative">
-                    <img src="{{ asset('icons/map_mosque.svg') }}" width="18" height="18" alt="" class="absolute top-1/2 -translate-y-1/2 left-2.5">
-                    <input type="search" name="search" value="{{ request('search') }}" placeholder="Cari Walikelas..." class="bg-white rounded-full pl-9 pr-2.5 py-1 border border-primary">
-                </div>
-                <button class="bg-primary rounded-full px-5 py-2.5 text-white text-xs font-semibold">
-                    Cari
-                </button>
-                @if(request('search'))
-                    <a href="{{ route('walikelas.index') }}" class="bg-gray-500 text-white rounded-full px-5 py-2.5 text-xs font-semibold">
-                        Reset
-                    </a>
-                @endif
-            </form>
         </div>
         <div class="mt-5 overflow-x-scroll">
             <table class="min-w-full w-max">
                 <thead>
                     <tr>
-                        <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium">No.</th>
-                        <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Kode Wali Kelas</th>
-                        <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Nama Wali Kelas</th>
-                        <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Kode Kelas</th>
-                        <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Aksi</th>
+                        <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium">No.</th>
+                        <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Kode Wali Kelas</th>
+                        <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Nama Wali Kelas</th>
+                        <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Kode Kelas</th>
+                        <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($data as $key => $item)
                     <tr>
-                        <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium text-center">{{ $key + 1 }}</td>
-                        <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->kode }}</td>
-                        <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->nama }}</td>
-                        <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->kode_kelas }}</td>
-                        <td class="flex items-center justify-center gap-4 bg-[#F5F5F5] px-6 py-2.5 text-sm font-medium border-l border-l-[#D9D9D9]">
+                        <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium text-center">{{ $key + 1 }}</td>
+                        <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->kode }}</td>
+                        <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->nama }}</td>
+                        <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $item->kode_kelas }}</td>
+                        <td class="flex items-center justify-center gap-4 bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-sm font-medium border-l border-l-[#D9D9D9]">
                             <a href="{{ route('walikelas.edit', $item->id) }}" class="text-[#0062FF] text-sm font-medium">Edit</a>
                             <p>|</p>
                             <form action="{{ route('walikelas.destroy', $item->id) }}" method="POST">
