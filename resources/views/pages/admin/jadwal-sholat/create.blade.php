067d30a6736347e827f58e7931f2a5fce97f97dd
adesopiann <adesopian884@gmail.com>
2025-03-01 20:25:38 +0700

    feat: add mobile responsive

--- resources/views/pages/admin/jadwal-sholat/create.blade.php
+++ resources/views/pages/admin/jadwal-sholat/create.blade.php
@@ -1,76 +1,71 @@
 @extends('components.layouts.admin.container')
+@section('title', 'Jadwal Sholat')
 
 @section('main')
-
-    <div class="flex space-x-8">
-        <section>
+    <section>
+        <div class="flex space-x-2 md:space-x-8 mb-6 md:mb-12">
+                <a href="{{ route('jadwal-sholat.index') }}" class="flex items-center gap-4">
+                    <div class="flex items-center justify-center bg-primary aspect-square rounded-md size-6">
+                        <img src="{{ asset('icons/chevron-left.svg') }}" width="8" height="8" alt="">
+                    </div>
+                    <p class="text-base font-bold text-mist">Jadwal Sholat</p>
+                </a>
             <a href="{{ route('jadwal-sholat.index') }}" class="flex items-center gap-4">
-                <div class="flex items-center justify-center bg-primary aspect-square rounded-md size-6">
-                    <img src="{{ asset('icons/chevron-left.svg') }}" width="8" height="8" alt="">
-                </div>
-                <p class="text-base font-bold text-mist">Jadwal Sholat</p>
+                <img src="{{ asset('icons/ep_arrow-right-bold.svg') }}" width="8" height="8" alt="" class="w-4">
             </a>
-        </section>
-        
-        <a href="{{ route('jadwal-sholat.index') }}" class="flex items-center gap-4">
-            <img src="{{ asset('icons/ep_arrow-right-bold.svg') }}" width="8" height="8" alt="" class="w-4">
-        </a>
-
-        <p class="text-base font-bold">Tambah Jadwal Baru</p>
-    </div>
-
-    <section class="mt-11">
-        <form action="{{ route('jadwal-sholat.store') }}" method="POST" class="bg-white p-15 space-y-6 rounded-md border-[1.5px] border-[#D9D9D9] max-w-[750px]">
+            <p class="text-base font-bold">Tambah Jadwal Sholat</p>
+        </div>
+        <form action="{{ route('jadwal-sholat.store') }}" method="POST" class="bg-white p-4  md:p-15 space-y-3 md:space-y-6 rounded-md border-[1.5px] border-[#D9D9D9] max-w-[750px]">
             @csrf
             @method('POST')
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="tanggal" class="text-lg font-medium">Tanggal</label>
-                <input type="date" name="tanggal" id="tanggal" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('tanggal') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="tanggal" class="text-sm md:text-lg font-medium">Tanggal</label>
+                <input type="date" name="tanggal" id="tanggal" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('tanggal') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="imsak" class="text-lg font-medium">Imsak</label>
-                <input type="time" name="imsak" id="imsak" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('imsak') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="imsak" class="text-sm md:text-lg font-medium">Imsak</label>
+                <input type="time" name="imsak" id="imsak" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('imsak') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="subuh" class="text-lg font-medium">Subuh</label>
-                <input type="time" name="subuh" id="subuh" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('subuh') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="subuh" class="text-sm md:text-lg font-medium">Subuh</label>
+                <input type="time" name="subuh" id="subuh" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('subuh') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="terbit" class="text-lg font-medium">Terbit</label>
-                <input type="time" name="terbit" id="terbit" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('terbit') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="terbit" class="text-sm md:text-lg font-medium">Terbit</label>
+                <input type="time" name="terbit" id="terbit" class="col-span-3 bg-white px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('terbit') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="dhuha" class="text-lg font-medium">Dhuha</label>
-                <input type="time" name="dhuha" id="dhuha" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('dhuha') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="dhuha"class="text-sm md:text-lg font-medium">Dhuha</label>
+                <input type="time" name="dhuha" id="dhuha" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('dhuha') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="dzuhur" class="text-lg font-medium">Dzuhur</label>
-                <input type="time" name="dzuhur" id="dzuhur" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('dzuhur') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="dzuhur" class="text-sm md:text-lg font-medium">Dzuhur</label>
+                <input type="time" name="dzuhur" id="dzuhur" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('dzuhur') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="ashar" class="text-lg font-medium">Ashar</label>
-                <input type="time" name="ashar" id="ashar" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('ashar') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="ashar" class="text-sm md:text-lg font-medium">Ashar</label>
+                <input type="time" name="ashar" id="ashar" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('ashar') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="maghrib" class="text-lg font-medium">Maghrib</label>
-                <input type="time" name="maghrib" id="maghrib" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('maghrib') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="maghrib" class="text-sm md:text-lg font-medium">Maghrib</label>
+                <input type="time" name="maghrib" id="maghrib" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('maghrib') {{ $message }} @enderror</p>
             </div>
-            <div class="grid grid-cols-4 gap-3 items-center">
-                <label for="isya" class="text-lg font-medium">Isya</label>
-                <input type="time" name="isya" id="isya" class="col-span-3 bg-white px-5 py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
-                <p class="col-start-2 text-red-500 text-sm">@error('isya') {{ $message }} @enderror</p>
+            <div class="grid grid-cols-3 md:grid-cols-4 gap-2 md:gap-3 items-center">
+                <label for="isya" class="text-sm md:text-lg font-medium">Isya</label>
+                <input type="time" name="isya" id="isya" class="col-span-3 bg-white  px-3 py-2 md:px-5 md:py-3.5 rounded-md border-[0.5px] border-[#D9D9D9]">
+                <p class="col-start-2 text-red-500 text-[12px] md:text-sm">@error('isya') {{ $message }} @enderror</p>
             </div>
             <div class="flex items-center justify-end gap-4 pt-4">
-                <a href="{{ route('jadwal-sholat.index') }}" class="bg-mist py-2 px-2.5 rounded text-white text-sm font-medium">Batal</a>
-                <button type="submit" class="bg-primary py-2 px-2.5 rounded text-white text-sm font-medium">Simpan</button>
+                <a href="{{ route('jadwal-sholat.index') }}" class="bg-mist md:py-2 px-5 md:px-2.5 rounded text-white text-sm font-medium">Batal</a>
+                <button type="submit" class="bg-primary md:py-2 px-5 md:px-2.5 rounded text-white text-sm font-medium">Simpan</button>
             </div>
         </form>
     </section>
--- resources/views/pages/admin/jadwal-sholat/index.blade.php
+++ resources/views/pages/admin/jadwal-sholat/index.blade.php
@@ -1,19 +1,31 @@
 @extends('components.layouts.admin.container')
+@section('title', 'Jadwal Sholat')
 
 @section('main')
-<section class="flex space-x-8">
-    <section>
+<section>
+    <div class="flex space-x-2 md:space-x-8 mb-6 md:mb-12">
         <a href="{{ route('admin.home') }}" class="flex items-center gap-4">
             <div class="flex items-center justify-center bg-primary aspect-square rounded-md size-6">
                 <img src="{{ asset('icons/chevron-left.svg') }}" width="8" height="8" alt="">
             </div>
-            <p class="text-base font-bold">Jadwal Sholat</p>
+            <p class="text-sm md:text-base font-bold">Jadwal Sholat</p>
         </a>
-    </section>
-</section>
-
-<section class="mt-11">
-    <div class="flex items-center justify-between">
+    </div>
+    <div class="flex flex-col space-y-6 md:space-y-0 md:items-center md:justify-between md:flex-row">
+        <form class="flex items-center gap-2.5 w-full md:w-auto">
+            <div class="relative w-full md:w-auto">
+                <img src="{{ asset('icons/map_mosque.svg') }}" width="18" height="18" alt="" class="absolute top-1/2 -translate-y-1/2 left-2.5">
+                <input type="search" name="q" value="{{ request('q') }}" placeholder="Cari Jadwal..." class="w-full md:w-auto bg-white rounded-full pl-9 pr-2.5 py-1 border border-primary">
+            </div>
+            <button type="submit" class="bg-primary rounded-full px-5 py-2.5 text-white text-xs font-semibold">
+                Cari
+            </button>
+            @if(request('q'))
+                <a href="{{ route('jadwal-sholat.index') }}" class="bg-gray-500 text-white rounded-full px-5 py-2.5 text-xs font-semibold">
+                    Reset
+                </a>
+            @endif
+        </form>
         <div class="flex items-center gap-2.5">
             <a href="{{ route('jadwal-sholat.create') }}" class="flex items-center gap-4 px-5 py-2 rounded bg-transparent border border-primary text-primary">
                 <img src="{{ asset('icons/map_mosque.svg') }}" alt="mosque icon" width="20" height="20">
@@ -25,46 +37,37 @@
             ])
         </div>
         @include('components.shared.tables.import-error')
-        <form class="flex items-center gap-2.5">
-            <div class="relative">
-                <img src="{{ asset('icons/map_mosque.svg') }}" width="18" height="18" alt="" class="absolute top-1/2 -translate-y-1/2 left-2.5">
-                <input type="search" name="q" value="{{ request('q') }}" class="bg-white rounded-full pl-9 pr-2.5 py-1 border border-primary">
-            </div>
-            <button type="button" class="bg-primary rounded-full px-5 py-2.5 text-white text-xs font-semibold">
-                Cari
-            </button>
-        </form>
     </div>
     <div class="mt-5 overflow-x-scroll">
         <table class="min-w-full w-max">
             <thead>
                 <tr>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium">No.</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Tanggal</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Imsak</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Subuh</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Terbit</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Dhuha</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Dzuhur</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Ashar</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Maghrib</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Isya</th>
-                    <th class="bg-primary px-6 py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Aksi</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium">No.</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Tanggal</th>
+                    <th class="bg-primary  px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Imsak</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Subuh</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Terbit</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Dhuha</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Dzuhur</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Ashar</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Maghrib</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Isya</th>
+                    <th class="bg-primary px-4 py-2 md:px-6 md:py-2.5 text-white text-sm font-medium border-l border-l-[#D9D9D9]">Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ( $jadwal_sholat as $jadwal)                   
                 <tr>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium text-center">{{ $loop->iteration }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['tanggal'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['imsak'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['subuh'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['terbit'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['dhuha'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['dzuhur'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['ashar'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['maghrib'] }}</td>
-                    <td class="bg-[#F5F5F5] px-6 py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['isya'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium text-center">{{ $loop->iteration }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['tanggal'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['imsak'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['subuh'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['terbit'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['dhuha'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['dzuhur'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['ashar'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['maghrib'] }}</td>
+                    <td class="bg-[#F5F5F5] px-4 py-2 md:px-6 md:py-2.5 text-black text-sm font-medium border-l border-l-[#D9D9D9]">{{ $jadwal['isya'] }}</td>
                     <td class="flex items-center justify-center gap-4 bg-[#F5F5F5] px-6 py-2.5 text-sm font-medium border-l border-l-[#D9D9D9]">
                         <a href="{{ route('jadwal-sholat.edit', $jadwal['id']) }}" class="text-[#0062FF] text-sm font-medium">Edit</a>
                         <p>|</p>
